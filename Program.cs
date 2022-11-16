910d50dc24f34ee777a244c72b94e45d8607c8b9
Luiz HenriqueENS <luizhenrique.egydio@gmail.com>
2022-11-16 10:01:30 -0300

    criado o metodo de login no SED

--- Program.cs
+++ Program.cs
@@ -1,14 +1,57 @@
-﻿using OpenQA.Selenium.Chrome;
-using WebDriverManager;
-using WebDriverManager.DriverConfigs.Impl;
-using WebDriverManager.Helpers;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Linq;
 
 namespace AlteradorDeSenhaSED;
 class Program
 {
     static void Main(string[] args)
     {
-        new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+        ChromeDriver driver = Sessao.IniciarSessao();
 
+        driver.Navigate().GoToUrl("https://sed.educacao.sp.gov.br");
+        FazerLoginNoSED(driver);
     }
+
+   #region LOGIN
+     private static void FazerLoginNoSED(ChromeDriver driver)
+     {
+         // fazer o login
+         System.Console.Write("Digite seu login: ");
+         string? login = Console.ReadLine();
+ 
+         while (login == "")
+         {
+             System.Console.Write("Você precisa digitar o login: ");
+             login = Console.ReadLine();
+         }
+ 
+         var campoLogin = Sessao.Procurar(driver, Tipos.XPATH, "//*[@id='name']");
+         campoLogin.SendKeys(login);
+ 
+         // SENHA
+         System.Console.Write("Digite seu senha: ");
+         string? senha = Console.ReadLine();
+ 
+         while (senha == "")
+         {
+             System.Console.Write("Você precisa digitar o login: ");
+             senha = Console.ReadLine();
+         }
+ 
+         var campoSenha = Sessao.Procurar(driver, Tipos.XPATH, "//*[@id='senha']");
+         campoSenha.SendKeys(senha);
+ 
+         // confirmar e entrar
+         var botao = Sessao.Procurar(driver, Tipos.XPATH, "//*[@id='botaoEntrar']");
+         botao.Click();
+ 
+         var modalCargosUL = Sessao.Procurar(driver, Tipos.XPATH, "//*[@id='sedUiModalWrapper_1body']/ul");
+         var cargos = modalCargosUL.FindElements(By.TagName("li"));
+         var proatec = cargos.First(x => x.FindElement(By.TagName("a")).GetAttribute("innerHTML") == "PROATEC");
+         proatec.Click();
+ 
+         System.Console.WriteLine("Login feito com sucesso!");
+     }
+   #endregion
 }
---
+++ Sessao.cs
@@ -0,0 +1,38 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
+
+class Sessao
+{
+    // inicia e configura sessao do ChromeDriver
+    public static ChromeDriver IniciarSessao()
+    {
+
+        // baixa automaticamente a versão compatível do ChromeDrive para o pc
+        new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+        ChromeDriver driver = new ChromeDriver();
+
+        return driver;
+    }
+    
+    public static IWebElement Procurar(ChromeDriver driver, Tipos tipo, string caminho)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60.0)) { PollingInterval = TimeSpan.FromSeconds(5.0) };
+
+        IWebElement? elemento = null;
+
+        switch (tipo)
+        {
+            case Tipos.XPATH:
+                elemento = wait.Until(x => x.FindElement(By.XPath(caminho)));
+                break;
+        }
+        return elemento;
+    }
+
+
+
+}

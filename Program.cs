01a47eca57f7fb501cc75917caada591dded2e50
Luiz HenriqueENS <luizhenrique.egydio@gmail.com>
2022-11-16 09:13:38 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
+
+namespace AlteradorDeSenhaSED;
+class Program
+{
+    static void Main(string[] args)
+    {
+        new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+
+    }
+}
